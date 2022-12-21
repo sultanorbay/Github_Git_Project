85832ef623e21abf4bea8abc39a416e2d0b62ba1
Sultan Orbay <orbaysultan@gmail.com>
2022-12-21 22:25:09 +0300

    added

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -2,6 +2,7 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("Hello Test");
         System.out.println("About to share project on Github");
+        System.out.println("---------------");
     }
 
 }
